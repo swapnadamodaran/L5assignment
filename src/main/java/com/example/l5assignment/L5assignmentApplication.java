211ee65c88b2b2c5b92929fcb1b2c74420e5dc8e
swapna <swapna@gmail.com>
2024-06-02 19:06:30 -0500

    First commit Maven project

---
+++ src/main/java/com/example/l5assignment/L5assignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.l5assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L5assignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L5assignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/l5assignment/L5assignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.l5assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L5assignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
